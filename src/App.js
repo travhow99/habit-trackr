36bb8ec90e7ea1fd9076fc8c931d8ae1d44b70d5
travis <trav.horn@gmail.com>
2021-03-21 19:46:55 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import Todo from "./components/Todo";
+
+import DATA from './data';
+
+const tasks = DATA;
+
+console.log('ts', tasks);
+
+function addTask(name) {
+  alert(name);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>Habit Trackr</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {/* 
+          @todo loop through todos
+        */}
+        {tasks.map((task, index) => (
+          <Todo key={index} name={task.name} completed={task.completed} id={task.id} />
+        ))}
+      </ul>
     </div>
   );
 }
